ab42aafbc538d81c7bf78c021d746696339d2b80
stulzq <stulzq@qq.com>
2020-12-01 16:19:43 +0800

    Add project files.

---
+++ sample/QRConsole.SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QRConsole.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qr= new QRConsole();
+            qr.Output("aaaa");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/QRConsole/QRConsole.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using QRCoder;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace QRConsole
+{
+    public class QRConsole
+    {
+        public static void Output(string text)
+        {
+            const int threshold = 180;
+
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new BitmapByteQRCode(qrCodeData); 
+            var bitmapBytes = qrCode.GetGraphic(1);
+
+            var image = Image.Load<Rgba32>(bitmapBytes);
+            for (var i = 0; i < image.Width; i++)
+            {
+                for (var j = 0; j < image.Height; j++)
+                {
+                    //获取该像素点的RGB的颜色
+                    var color = image[i, j];
+                    if (color.B <= threshold)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("  ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                        Console.ResetColor();
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+    }
+}
